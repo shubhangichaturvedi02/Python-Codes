677ff8a10f5f5ca49c75e192bbc8d1f53ea1c140
shubhangi02 <chaturvedi.shubhangi02@gmail.com>
2024-12-29 22:38:00 +0530

    pre order traversal in binary tree

---
+++ Tree/binary_tree.py
@@ -0,0 +1,25 @@
+class TreeNode():
+   def __init__(self, data):
+      self.data = data
+      self.left_child = None
+      self.right_child = None
+
+
+new_tree = TreeNode('a')
+left_child = TreeNode('b')
+right_child = TreeNode('c')
+
+new_tree.left_child = left_child
+new_tree.right_child = right_child
+
+def pre_order_traversal(root_node):
+   if not root_node:
+      return
+   
+   print(root_node.data)
+
+   pre_order_traversal(root_node.left_child)
+   pre_order_traversal(root_node.right_child)
+
+
+pre_order_traversal(new_tree)
